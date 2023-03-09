8cc90dc160ceae148bc46364fc4a2b4a521ec911
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-09 17:54:13 +0100

    saved

--- controller/testController.ts
+++ controller/testController.ts
@@ -14,7 +14,7 @@ export const registerUser = async (
       email,
       password,
     });
-    emailEnv(newUser)
+    await emailEnv(newUser)
       .then((res) => console.log("this is res", res))
       .catch((err) => console.log("this is err", err));
     return res.status(200).json({
