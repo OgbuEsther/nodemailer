db60da945687a17feaf63a058d4a60c6f9ee7878
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-09 16:55:26 +0100

    saved user controller

---
+++ controller/testController.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from "express";
+
+import testModel from "../model/test";
+
+export const registerUser = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  try {
+    const { name, email, password } = req.body;
+
+    const user = await testModel.create({
+      name,
+      email,
+      password,
+    });
+    return res.status(200).json({
+      message: "user created successfully",
+      data: user,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      message: "an error",
+      data: error,
+    });
+  }
+};
