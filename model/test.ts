f7b212eee2519fbe311a46ec95a6b9e3a76df008
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-09 16:51:25 +0100

    user model completed

--- model/test.ts
+++ model/test.ts
@@ -1,1 +1,24 @@
 import mongoose from "mongoose";
+
+interface user {
+  name: string;
+  email: string;
+  password: string;
+}
+
+interface IUser extends user, mongoose.Document {}
+
+const testSchema = new mongoose.Schema<user>({
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+});
+
+const testModel = mongoose.model<IUser>("user", testSchema);
+export default testModel;
