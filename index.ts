6cc9cbf1a2cac9758aa5e539e9830a3536d76d98
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-09 16:57:14 +0100

    saved user routes

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 import express, { Application } from "express";
 require("./config/db");
 import cors from "cors";
+import userRoutes from "./routes/userRoutes";
 const PORT: number = 8888;
 
 const app: Application = express();
 app.use(express.json());
 app.use(cors());
+app.use("/api", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`server is up on port ${PORT}`);
